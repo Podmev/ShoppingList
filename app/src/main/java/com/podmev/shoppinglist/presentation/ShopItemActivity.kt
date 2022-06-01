98db6f00c30d33db96db08ffb52055cda8110c41
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-06-02 00:24:55 +0300

    add activity_shop_item.xml, and empty ShopItemActivity

---
+++ app/src/main/java/com/podmev/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.podmev.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.podmev.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
