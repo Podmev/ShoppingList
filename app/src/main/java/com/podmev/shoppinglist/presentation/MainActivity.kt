25b3373c1337d5c818731f19bf6aa76724ac2252
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-06-01 21:46:48 +0300

    made getItemViewType with 2 types, set size for recycledViewPool

--- app/src/main/java/com/podmev/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/podmev/shoppinglist/presentation/MainActivity.kt
@@ -23,6 +23,11 @@ class MainActivity : AppCompatActivity() {
     private fun setupRecyclerView(){
         val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
         adapter = ShopListAdapter()
-        rvShopList.adapter = adapter
+        with(rvShopList){
+            adapter = adapter
+            //выставляем сколько свободных viewholder для каждого типа view
+            recycledViewPool.setMaxRecycledViews(ShopListAdapter.ENABLED_VIEW, ShopListAdapter.MAX_POOL_SIZE)
+            recycledViewPool.setMaxRecycledViews(ShopListAdapter.DISABLED_VIEW, ShopListAdapter.MAX_POOL_SIZE)
+        }
     }
 }
--- app/src/main/java/com/podmev/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/podmev/shoppinglist/presentation/ShopListAdapter.kt
@@ -1,5 +1,6 @@
 package com.podmev.shoppinglist.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.podmev.shoppinglist.R
 import com.podmev.shoppinglist.domain.ShopItem
+import java.lang.RuntimeException
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
     var shopList = listOf<ShopItem>()
@@ -15,28 +17,25 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
             field = value
             notifyDataSetChanged()
         }
+    var count = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_shop_disabled,
-            parent,
-            false
-        )
+        Log.d("ShopListAdapter", "onCreateViewHolder: count=${++count}")
+        val layout = when(viewType){
+            ENABLED_VIEW -> R.layout.item_shop_enabled
+            DISABLED_VIEW -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown viewType: $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent,false)
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         val shopItem = shopList[position]
-        val status = if(shopItem.enabled) "Active" else "Not Active"
-        viewHolder.tvName.text = "${shopItem.name} ${status}"
+        viewHolder.tvName.text = shopItem.name
         viewHolder.tvCount.text = shopItem.count.toString()
         viewHolder.view.setOnLongClickListener {
             true
-        }
-        if(shopItem.enabled){
-            viewHolder.tvName.text = "${shopItem.name} ${status}"
-            viewHolder.tvCount.text = shopItem.count.toString()
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.holo_red_light))
         }
     }
 
@@ -45,11 +44,11 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
         super.onViewRecycled(viewHolder)
         viewHolder.tvName.text = ""
         viewHolder.tvCount.text = ""
-        viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.white))
     }
 
     override fun getItemViewType(position: Int): Int {
-        return position
+        val shopItem = shopList[position]
+        return if(shopItem.enabled) ENABLED_VIEW else DISABLED_VIEW
     }
 
     override fun getItemCount(): Int {
@@ -59,5 +58,12 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName = view.findViewById<TextView>(R.id.tv_name)
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    companion object{
+        const val ENABLED_VIEW = 101
+        const val DISABLED_VIEW = 102
+
+        const val MAX_POOL_SIZE = 15
     }
 }
