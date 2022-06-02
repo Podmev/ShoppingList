dc00c13c73c683f16619ca57087b91b953b02945
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-06-03 01:07:26 +0300

    init fragment from activity, using transaction

--- app/src/main/java/com/podmev/shoppinglist/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/podmev/shoppinglist/presentation/ShopItemFragment.kt
@@ -67,7 +67,8 @@ class ShopItemFragment(
             tilName.error = message
         }
         viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
-            finish()
+            //activity can be absent
+            activity?.onBackPressed()
         }
     }
 
@@ -136,6 +137,14 @@ class ShopItemFragment(
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
         private const val MODE_UNKNOWN = ""
+
+        fun newInstanceAddItem(): ShopItemFragment{
+            return ShopItemFragment(MODE_ADD)
+        }
+
+        fun newInstanceEditItem(shopItemId: Int): ShopItemFragment{
+            return ShopItemFragment(MODE_EDIT, shopItemId)
+        }
 
         fun newIntentAddItem(context: Context): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
